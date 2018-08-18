fba8ee31b73ca9e6216e0a980dfbf33b0b09ba82
rern <rernrern@gmail.com>
2018-08-18 17:53:43 +0700

    Update uninstall_RuneYoutube.sh

--- usr/local/bin/uninstall_RuneYoutube.sh
+++ usr/local/bin/uninstall_RuneYoutube.sh
@@ -1,10 +1,12 @@
+
 #!/bin/bash
 
 # main reference
-alias=RuneYoutube
+alias=yout
 
 ### template - import default variables, functions
 . /srv/http/addonstitle.sh
+. /srv/http/addonsedit.sh
 
 ### template - function: start message, installed check
 uninstallstart $@
@@ -21,27 +23,21 @@ echo -e "$bar Remove shell scripts..."
 rm -v /usr/local/bin/tube
 rm -v /usr/local/bin/tubeplaylist
 
-echo -e "$bar Reverting Patched files..."
-file=/srv/http/app/templates/playback.php
-echo $file
-sed -i $'/<!-- RUNE_YOUTUBE_MOD -->/,/<!-- END_RUNE_YOUTUBE_MOD -->/ d' $file
-file=/srv/http/app/templates/header.php
-echo $file
-sed -i $'/<!-- RUNE_YOUTUBE_MOD -->/,/<!-- END_RUNE_YOUTUBE_MOD -->/ d' $file
-
-# for RuneUI Enhancement
-file=/srv/http/app/templates/playbackcustom.php
-[[ -e $file ]] && sed -i '/id="pl-import-youtube"/ {s/^/<!--/; s/$/-->/}' $file
-
-file=/srv/http/app/templates/footer.php
-echo $file
-sed -i -e '/RuneYoutube.js/ d' $file
-
 echo -e "$bar Removing youtube javascript.."
 rm -v /srv/http/assets/js/RuneYoutube.js
 
 echo -e "$bar Removing youtube directory.."
 rm -r /mnt/MPD/LocalStorage/Youtube
+
+echo -e "$bar Reverting Patched files..."
+
+[[ -e /srv/http/app/templates/playback.php.backup ]] && backup=.backup
+files="
+/srv/http/app/templates/playback.php$backup
+/srv/http/app/templates/footer.php$backup
+"
+restorefile $file
+
 # end custom script --------------------------------------------------------------------------------<<<
 
 ### template - function: remove version from database, finish message
